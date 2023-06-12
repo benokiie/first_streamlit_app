a83e606bee3151608375d4c7bec91a4d21b15790
Verino Valgriel <135976298+benokiie@users.noreply.github.com>
2023-06-13 06:33:22 +0800

    Update streamlit_app.py
    Changed title to Mom's New

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 #Created the main python file
 import streamlit
 
-streamlit.title("My Parents New Healthy Diner")
+streamlit.title("My Mom's New Healthy Diner")
 
 streamlit.header("Breakfast Favorites")
 
