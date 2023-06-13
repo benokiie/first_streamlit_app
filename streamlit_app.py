6a32f2731b800b9c37f74bdd6dd6e565daee68ce
Verino Valgriel <135976298+benokiie@users.noreply.github.com>
2023-06-13 18:14:58 +0800

    Update streamlit_app.py
    Added
    import requests
    # New section to display
    streamlit.header("Fruityvice Fruit Advice!")
    
    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
    streamlit.text(fruityvice_response)

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 #Created the main python file
 import streamlit
 import pandas
+import requests
 
 streamlit.title("My Mom's New Healthy Diner")
 
@@ -22,3 +23,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+# New section to display
+streamlit.header("Fruityvice Fruit Advice!")
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
